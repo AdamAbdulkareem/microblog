9683e6ff14243a8dc9fb1e624868741dff655758
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-07-28 12:16:01 +0100

    Modify some files

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import flask_app
+
 
 # Define the routes and their view functions
 @flask_app.route('/')
 @flask_app.route('/index')
 def index():
-    return "Hello, Engr. Adam Abdulkareem"
+    user = {"username" : "Engr. Abdulkareem"}
+    posts = [
+        {
+            'author': {'username': 'Adam'},
+            'body': 'Beautiful day in Ibadan'
+        },
+        {
+            'author': {'username': 'Dare'},
+            'body': 'The Avengers movie was so cool'
+        }
+    ]
+    
+    return render_template('index.html', title = "Home",user=user, posts=posts)
