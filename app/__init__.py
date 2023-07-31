7c001cf92091c7b0ec9a72d832da5541153709c7
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-07-31 11:15:20 +0100

    Modify some files

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 # Create the Flask application instance
 flask_app = Flask(__name__)
+flask_app.config.from_object(Config)
 
 # Import the routes module
 from app import routes
