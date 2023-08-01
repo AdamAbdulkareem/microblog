25a9e31542fcc61373ee7137a78f216b1c59655a
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-08-01 17:13:26 +0100

    Add some files

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Create the Flask application instance
 flask_app = Flask(__name__)
 flask_app.config.from_object(Config)
+db = SQLAlchemy(flask_app)
+migrate = Migrate(flask_app, db)
 
 # Import the routes module
-from app import routes
+from app import routes, models
